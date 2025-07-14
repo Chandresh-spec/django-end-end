1997b414c74511fde59268e6fcffc4fdd7bdd49b
Chandresh-spec <chandumoger819@gmail.com>
2025-07-14 23:40:38 +0530

    day2completed

--- 30-days-django/beginner/randomjoke/urls.py
+++ 30-days-django/beginner/randomjoke/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views. random_joke,name="random_joke"),
+    path('jokes2/',views. random_joke2,name="random_joke2"),
+    path('jokes2/',views. random_joke2,name="random_joke2"),
+    path('quote/',views.quote_OfThe_Day,name="quote_OfThe_Day"),
+    
     
 ]
--- 30-days-django/beginner/randomjoke/views.py
+++ 30-days-django/beginner/randomjoke/views.py
@@ -11,3 +11,23 @@ def random_joke(request):
     joke=random.choice(jokes)
     return render(request,"joke.html",{'joke':joke})
 
+def random_joke2(request):
+    joke=random.choice(jokes)
+
+    return render(request,'joke2.html',{'joke':joke})
+
+quotes=[
+    "Be yourself; everyone else is already taken",
+    "The only way to do great work is to love what you do.",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    "Don’t watch the clock; do what it does. Keep going.",
+    "In the middle of every difficulty lies opportunity.",
+    "Do something today that your future self will thank you for.",
+    "The best way to predict the future is to create it.",
+
+
+]
+def quote_OfThe_Day(request):
+    quote=random.choice(quotes)
+    return render(request,'app/layout.html',{'quote':quote})
+
